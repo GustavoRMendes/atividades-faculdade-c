86c449636bee885b55ffcbad0ba2a5292360cdb0
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-11 11:16:43 -0400

    Add files via upload

---
+++ Arrays.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	string nomes[] = {"Gustavo","Felipe"};
+	for (int cont = 0; cont < 1; cont++) {
+		cout << nomes[cont] << " \n";
+	}
+	return 0;
+}
