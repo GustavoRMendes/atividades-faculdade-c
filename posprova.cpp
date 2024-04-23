fdc633d563d066b617b15ff5b9e5610618701c9b
Gustavo Mendes <gustavormendess@gmail.com>
2024-04-22 21:56:13 -0400

    Add files via upload

---
+++ posprova.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	string texto1 = "Ola, me chamo ";
+	string texto2 = "Gustavo!";
+		texto1 += texto2;
+	cout << texto1 << endl;
+	return 0;
+}
