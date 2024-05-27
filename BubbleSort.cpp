390d1e2054419ec48cb6984f37d114a02855d713
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-27 19:43:08 -0400

    Bubble Sort c++

---
+++ BubbleSort.cpp
@@ -0,0 +1,11 @@
+//Aula de Algoritmos de Ordenação - 27/05/2024
+//
+// Bubble Sort
+//  Pequenos conjuntos de dados, compara o vizinho. Parecido com a busca linear, só que ele vai ordenando até chegar o final.
+//  Não exige muita memória, e é mais simples.
+//  Lista de pequenos elementos e não requer memória adicional.
+//  Não é eficiente, tem limitação, e é mais lento.
+//  Não é adptativo.
+//  Desempenho no pior caso, faz muitas comparações e trocas possíveis, tornando-o inefeciente.
+//  Não é estável.
+//  Ele recomeça novamente, e ordena o vetor.
