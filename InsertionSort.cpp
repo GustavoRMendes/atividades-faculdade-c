07ec33c4c064618d8bd6e7ff161dd9a1277cfa6d
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-27 20:53:25 -0400

    Insertion Sort c++

---
+++ InsertionSort.cpp
@@ -0,0 +1,8 @@
+//Insertion Sort
+//  Classifica a coleção de elementos, é uma escolha para um pequeno conjunto de dados.
+//  Ele é simples, é eficiente para pequeno conjuntos, e é in-place.
+//  Não é bom para grandes conjuntos
+//  Inicialização vai no início do vetor e vai comparar
+//  Iteração começa da esquerda pra direita 
+//  Inserção combinando quando um é maior e menor ele insere no local dele.
+//  Ordenar
