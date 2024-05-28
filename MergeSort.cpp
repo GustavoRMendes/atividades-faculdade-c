35ec2fe4872cfcf1c11cba960edf576bc95044de
Gustavo Mendes <gustavormendess@gmail.com>
2024-05-27 20:54:01 -0400

    Merge Sort c++

---
+++ MergeSort.cpp
@@ -0,0 +1,5 @@
+//Merge Sort
+//  Dividir para conquistar.
+//  Eficiente estável, divide, ordena e guarda.
+//  Ele lembra a árvore.
+//  Dividir, conquistar cada sublista recursivamente, e depois combinar as divisões.
