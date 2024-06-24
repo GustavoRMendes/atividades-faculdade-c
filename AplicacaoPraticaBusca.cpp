d01a9a814355fa363169c8d2344db52420839490
Gustavo Mendes <gustavormendess@gmail.com>
2024-06-24 16:37:44 -0400

    Add files via upload

---
+++ AplicacaoPraticaBusca.cpp
@@ -0,0 +1,21 @@
+//As aplicações práticas do algoritmo de busca, temos:
+//?
+//Pesquisa em Banco de Dados;
+//?
+//Classificação de Informações;
+//?
+//Verificar a existência de um item em um carrinho de compras online;
+//?
+//Encontrar um elemento em uma lista telefônica;
+//?
+//Verificar se um item existe em uma lista de contatos;
+//?
+//Encontrar um valor em uma matriz de pixels em processamento de imagem;
+//?
+//Pesquisar em Mecanismos de Busca na Web;
+//?
+//Aplicativos de Mapas e Navegação;
+//?
+//Localização de Dados em Bancos de Dados Espaciais;
+//?
+//Encontrar Itens em Mercados Online
